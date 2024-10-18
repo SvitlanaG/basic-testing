dc569523b8015809d95fab185c3790038d3296bf
Svitlana <svitlana.grytsai@gmail.com>
2024-10-18 23:32:09 +0200

    feat: implement simple, table and error-handling tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,67 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    //Arrange
+    const calculatorInput = { a: 4, b: 6, action: Action.Add };
+    //Act
+    const result = simpleCalculator({ ...calculatorInput });
+    //Assert
+    expect(result).toBe(10);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    //Arrange
+    const calculatorInput = { a: 7, b: 6, action: Action.Subtract };
+    //Act
+    const result = simpleCalculator({ ...calculatorInput });
+    //Assert
+    expect(result).toBe(1);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    //Arrange
+    const calculatorInput = { a: 5, b: 4, action: Action.Multiply };
+    //Act
+    const result = simpleCalculator({ ...calculatorInput });
+    //Assert
+    expect(result).toBe(20);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    //Arrange
+    const calculatorInput = { a: 12, b: 6, action: Action.Divide };
+    //Act
+    const result = simpleCalculator({ ...calculatorInput });
+    //Assert
+    expect(result).toBe(2);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    //Arrange
+    const calculatorInput = { a: 3, b: 3, action: Action.Exponentiate };
+    //Act
+    const result = simpleCalculator({ ...calculatorInput });
+    //Assert
+    expect(result).toBe(27);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    //Arrange
+    const calculatorInput = { a: 12, b: 6, action: 'plus' };
+    //Act
+    const result = simpleCalculator({ ...calculatorInput });
+    //Assert
+    expect(result).toBe(null);
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    //Arrange
+    const calculatorInput = { a: 'one', b: '6', action: Action.Exponentiate };
+    //Act
+    const result = simpleCalculator({ ...calculatorInput });
+    //Assert
+    expect(result).toBe(null);
   });
 });
--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,43 @@
 // Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  throwError,
+  throwCustomError,
+  resolveValue,
+  MyAwesomeError,
+  rejectCustomError,
+} from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    // Write your test here
+    const result = await resolveValue('value to resolve');
+    expect(result).toBe('value to resolve');
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
-    // Write your test here
+    expect(() => {
+      throwError('Error is ocured!');
+    }).toThrow('Error is ocured!');
   });
 
   test('should throw error with default message if message is not provided', () => {
-    // Write your test here
+    expect(() => {
+      throwError();
+    }).toThrow('Oops!');
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    // Write your test here
+    expect(() => {
+      throwCustomError();
+    }).toThrow();
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    // Write your test here
+    return expect(rejectCustomError()).rejects.toBeInstanceOf(MyAwesomeError);
   });
 });
